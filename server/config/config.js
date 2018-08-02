f2c29ee41f6a90665f4e3cef3ec5c52ee4ec6887
Juan Pablo <jprestrepo94@misena.edu.co>
2018-08-02 07:55:34 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV==='dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb://cafe-user:Ocgpablo98_@ds159631.mlab.com:59631/cafe-jpr';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
